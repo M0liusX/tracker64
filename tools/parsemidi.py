a4243b5569024f0c296a73c7d3b81817fd897e71
Mario Molina <maroiamolina1996@gmail.com>
2023-04-02 21:07:09 -0500

    Add exporting capabilities to libultra parsemidi tool

--- tools/parsemidi.py
+++ tools/parsemidi.py
@@ -71,6 +71,85 @@ AL_CMIDI_CNTRL_LOOPEND        = 103
 AL_CMIDI_CNTRL_LOOPCOUNT_SM   = 104
 AL_CMIDI_CNTRL_LOOPCOUNT_BIG  = 105
 
+
+class track64:
+   def __init__(self):
+      self.commands = []
+      self.size = 0
+      self.loop = 0
+      self.lstatus = 0
+   def delay_size(self, delay):
+      if (delay == 0): 
+         return 1
+      else:
+         return math.floor(math.log(delay, 128)) + 1
+   def add_command(self, command):
+      self.commands.append(command)
+      mesg = command[0]
+      extra = 0
+      if mesg == AL_MIDI_Meta:
+         self.lstatus = 0
+         extra = 1
+      elif self.lstatus != mesg:
+         self.lstatus = mesg
+         extra = 1
+      self.size += extra + len(command[1]) + len(encode_delta(command[2]))
+   def get_size(self):
+      return self.size
+   def init_loop(self):
+      self.loop = self.size
+   def get_loop_offset(self):
+      return self.size - self.loop
+   def save(self, file):
+      lstatus = 0
+      for command in self.commands:
+         # write delay
+         for byte in encode_delta(command[2]):
+            file.write(byte.to_bytes(1, 'big'))
+         
+         # write mesg
+         mesg = command[0]
+         if mesg == AL_MIDI_Meta:
+            file.write(mesg.to_bytes(1, 'big'))
+            lstatus = 0
+         elif lstatus != mesg:
+            file.write(mesg.to_bytes(1, 'big'))
+            lstatus = mesg
+
+         # write mesg data
+         for byte in command[1]:
+            file.write(byte.to_bytes(1, 'big'))
+
+
+class midi64:
+   def __init__(self):
+      self.tracks = [track64() for i in range(16)]
+      self.division = 0
+   def set_division(self, div):
+      self.division = div
+   def save(self, dir):
+      offset = 68
+      with open(dir, "wb") as f:
+         for t in self.tracks:
+            if t.get_size() == 0:
+               f.write(int(0).to_bytes(4, 'big'))
+            else:
+               assert(offset <= 4,294,967,295 ) # seq can't be this big
+               f.write(offset.to_bytes(4, 'big'))
+               offset += t.get_size()
+         f.write(self.division.to_bytes(4, 'big'))
+         for t in self.tracks:
+            if t.get_size() == 0:
+               continue
+            t.save(f)
+   def add_command(self, track, command):
+         self.tracks[track].add_command(command)
+   def init_loop(self, track):
+         self.tracks[track].init_loop()
+   def get_loop_offset(self, track):
+         return self.tracks[track].get_loop_offset()
+
+
 def read_bin(dir):
     with open(dir, "rb") as f:
         return bytearray(f.read())
@@ -121,23 +200,23 @@ def get_byte(track):
 
 def parse_midi():
    division = get_long(64)
+   midi.set_division(division)
    for i in range(0, 64, 4):
       curLoc[i >> 2] = get_long(i)
    
    print("Division: " + str(division))
-   parse_track(0, division)
-   # for t in trackOffsets:
-   #    parse_track(t)
+   for i in range(16):
+      parse_track(i, division)
 
 def parse_track(track, division):
    if curLoc[track] == 0: return
 
    lstatus = 0
-   ticks = 0
+   # ticks = 0
    while True:
-      delta = parse_delta(track)
-      ticks += delta
-      print(ticks/division, end=": ")
+      # delta = parse_delta(track)
+      # ticks += delta
+      # print(ticks/division, end=": ")
       lstatus, done = parse_command(track, lstatus)
       if done: break
 
@@ -152,6 +231,18 @@ def parse_delta(track):
             break
    return delta
 
+def encode_delta(delta):
+   bytes = []
+   c = 0x00
+   while delta >= 0x7F:
+      byte = (delta & 0x7F) | c
+      bytes.append(byte)
+      delta = delta >> 7
+      c = 0x80
+   bytes.append(delta | c)
+   bytes.reverse()
+   return bytes
+
 NOTES = ['C','C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#',' A', 'A#', 'B']
 def parse_note(value):
    key = value % 12
@@ -159,29 +250,40 @@ def parse_note(value):
    return NOTES[key] + str(pitch)
 
 def parse_command(track, lstatus):
+   delta = parse_delta(track)
    status = get_byte(track)
 
    if status == AL_MIDI_Meta:
       etype = get_byte(track)
+      status = 0
 
       if etype == AL_MIDI_META_TEMPO:
-         status = 0
-         tempo = (get_byte(track) << 16) + (get_byte(track) << 8) + get_byte(track)
+
+         b1 = get_byte(track); b2 = get_byte(track); b3 = get_byte(track);
+         tempo = (b1 << 16) + (b2 << 8) + b3
+         midi.add_command(track, (AL_MIDI_Meta, [AL_MIDI_META_TEMPO, b1, b2, b3], delta))
          print("Tempo: " + str(tempo))
       elif etype == AL_CMIDI_LOOPSTART_CODE:
-         get_byte(track); get_byte(track) # Two unused bytes
-         status = 0
+         b1 = get_byte(track); b2 = get_byte(track) # Two unused bytes
+         midi.add_command(track, (AL_MIDI_Meta, [AL_CMIDI_LOOPSTART_CODE, b1, b2], delta))
+         midi.init_loop(track)
          print("Loop Start")
       elif etype == AL_CMIDI_LOOPEND_CODE:
          # Skip 6 Bytes Used For Looping
          # 1   LoopCt
          # 2   curLpCt: 0xFF means loop forever
          # 3-6 offset (big endian)
-         get_byte(track); get_byte(track); get_byte(track);
-         get_byte(track); get_byte(track); get_byte(track);
-         status = 0
+         b1 = get_byte(track); b2 = get_byte(track); b3 = get_byte(track);
+         b4 = get_byte(track); b5 = get_byte(track); b6 = get_byte(track);
+         offset = midi.get_loop_offset(track) + len(encode_delta(delta)) + 8
+         b3 = (0xFF000000 & offset) >> 24
+         b4 = (0x00FF0000 & offset) >> 16
+         b5 = (0x0000FF00 & offset) >> 8
+         b6 = (0x000000FF & offset)
+         midi.add_command(track, (AL_MIDI_Meta, [AL_CMIDI_LOOPEND_CODE, b1,b2,b3,b4,b5,b6], delta))
          print("Loop END")
       elif etype == AL_MIDI_META_EOT:
+         midi.add_command(track, (AL_MIDI_Meta, [AL_MIDI_META_EOT, 0], delta))
          print("TRACK END")
          return 0, True
       else:
@@ -196,15 +298,22 @@ def parse_command(track, lstatus):
          status = lstatus
 
       mesg = status & 0xf0
+      channel = (status & 0x0f) + 1
+      print("Channel " + str(channel), end= ":: ")
+      assert( (byte   & 0x80) == 0) # first data byte always less than 0x80
+
       if mesg != AL_MIDI_ProgramChange and mesg != AL_MIDI_ChannelPressure:
          byte2 = get_byte(track)
 
       if mesg == AL_MIDI_NoteOn:
             duration = parse_delta(track)
+            midi.add_command(track, (status, [byte, byte2] + encode_delta(duration), delta))
             print("Note On: "+ parse_note(byte) + " V:" + str(byte2) + " D:" + str(duration))
       elif mesg == AL_MIDI_ProgramChange:
+            midi.add_command(track, (status, [byte], delta))
             print("Program Change: " + str(byte))
       elif mesg == AL_MIDI_ControlChange:
+         midi.add_command(track, (status, [byte, byte2], delta))
          if byte == AL_MIDI_VOLUME_CTRL:
             print("Control Change (Vol): " + str(byte2))
          elif byte == AL_MIDI_PAN_CTRL:
@@ -222,5 +331,8 @@ def parse_command(track, lstatus):
 
 
 ### MAIN ###
+
+midi = midi64()
 seq = read_bin("../samples/seq/rainbow.bin")
 parse_midi()
+midi.save("../samples/seq/rainbowexport.bin")
